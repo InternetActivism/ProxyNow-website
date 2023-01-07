279dbe1ac17d8e985321677e9b0106275b4e5cc1
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-01-06 18:46:09 -0800

    Removed duplicate styles folder and fixed index.tsx HTML structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
+import Head from 'next/head'
+import styles from 'src/styles/Home.module.css'
+
+
 export default function Home() {
   return (
-    <div className="pt-28 text-3xl font-semibold flex w-full justify-center">
-      You can help censorship around the world
-    </div>
+    <>
+      <Head>
+          <title>ProxyNow: Prevent Censorship</title>
+          <meta name="description" content="Prevent International Censorship" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className="pt-28 text-3xl font-semibold flex w-full justify-center">
+          You can help censorship around the world
+        </div>
+      </main>
+    </>
   );
 }
