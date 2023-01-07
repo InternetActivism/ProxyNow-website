8053c4a5a921cf74e69eda36a02df986fec35704
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-01-06 20:30:30 -0800

    Create CTA buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,43 +33,6 @@ const Header = () => {
         </div>
       </div>
       <div>
-        <Menu as="div" className="relative inline-block text-left">
-          <Menu.Button className="flex items-center gap-2 text-lg font-semibold text-black">
-            {language.name}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-5 h-5"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-            </svg>
-          </Menu.Button>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right overflow-hidden divide-y divide-gray-100 rounded bg-white shadow-md ring-1 ring-black ring-opacity-5 focus:outline-none">
-              {languages.map((lang) => (
-                <Menu.Item key={lang.code}>
-                  <button
-                    className="group flex w-full items-center px-2 py-2 text-base bg-white hover:bg-gray-100 transition-all"
-                    onClick={() => setLanguage(lang)}
-                  >
-                    {lang.name}
-                  </button>
-                </Menu.Item>
-              ))}
-            </Menu.Items>
-          </Transition>
-        </Menu>
       </div>
     </header>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,18 @@ export default function Home() {
         <div className="main-content">
           <div className="main-text">
             <div className="pt-40 pl-40 text-[82px] font-semibold flex w-2/3 justify-center">
-              <h1>Help millions access the <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#54A8EA] to-[#167CCB]">internet</span>.</h1>
+              <h1>Help millions access the <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#54A8EA] to-[#167CCB]">internet.</span></h1>
             </div>
             <p className="pt-5 pl-40 text-[24px] w-2/3">Easily find and deploy internet proxies to help people bypass internet censorship all over the world.</p>
           </div>
+          <button className="mt-10 ml-36 inline-flex bg-[#0085FF] hover:bg-blue-700 text-white text-[24px] font-semibold py-2 px-4 rounded-full">
+            Share With Friends
+            <svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M9.47 5.47a.75.75 0 0 1 1.06 0l6 6a.75.75 0 0 1 0 1.06l-6 6a.75.75 0 1 1-1.06-1.06L14.94 12 9.47 6.53a.75.75 0 0 1 0-1.06Z" fill="white"/></svg>
+          </button>
+          <button className="group proxy-button mt-10 ml-5 inline-flex bg-[white] hover:bg-[#0085FF] hover:text-white text-[#0085FF] text-[24px] border border-[#0085FF] font-semibold py-2 px-4 rounded-full">
+            View All Proxies
+            <svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path className="fill-current" fill-rule="evenodd" clip-rule="evenodd" d="M9.47 5.47a.75.75 0 0 1 1.06 0l6 6a.75.75 0 0 1 0 1.06l-6 6a.75.75 0 1 1-1.06-1.06L14.94 12 9.47 6.53a.75.75 0 0 1 0-1.06Z" fill="#0085FF"/></svg>
+          </button>
         </div>
       </main>
     </>
