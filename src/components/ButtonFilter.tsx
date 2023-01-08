4e0979d0cabfcdac1d8272175ab5cfa32bf7ce32
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-01-08 14:55:24 -0800

    Improve filter buttons

--- src/components/ButtonFilter.tsx
+++ src/components/ButtonFilter.tsx
@@ -1,17 +1,18 @@
+import Image from "next/image";
+
 const ButtonFilter = (props) => {
     var svg;
     if (props.svg) {
         svg = <div className="px-1">
-                {props.svg}
+                <Image src={props.svg} alt="logo" width={24} height={24} />
             </div>
-            
     } else {
         svg = <div className="pl-3 sm:pl-4 lg:pl-6"></div>
     }
 
     return(
         <>
-        <button className={`flex proxy-button ${props.fill} hover:${props.selectedColor} text-${props.textColor} py-2 rounded-xl w-44`}>
+        <button className={`flex proxy-button ${props.fill} ${props.hoverColor} ${props.textColor} ${props.hoverTextColor} ${props.selectedColor} ${props.selectedTextColor} py-2 rounded-xl w-28 sm:w-32 md:w-36 lg:w-44`}>
             <div className="flex items-center justify-center mx-auto">
                 {svg}
                 <div className="font-medium text-xs sm:text-sm md:text-md lg:text-lg pr-3 sm:pr-4 lg:pr-6">
--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -18,9 +18,11 @@ export default function List() {
         <div className="proxies pt-32">
           <div className="filter pl-[5%] flex flex-col">
             <h1 className="font-medium text-lg sm:text-xl md:text-2xl lg:text-3xl mb-4">Filter</h1>
-            <ButtonFilter text="All Proxies" fill="bg-[#F0F0F0]"/>
-            <ButtonFilter text="WhatsApp" fill="bg-[#F0F0F0]" textColor="[#25D366]"/>
-            <ButtonFilter text="Telegram" fill="bg-[#F0F0F0]" textColor="[#229ED9]"/>
+            <div className="flex flex-col gap-3">
+              <ButtonFilter text="All Proxies" fill="bg-[#F0F0F0]" hoverColor="hover:bg-[#0085FF]" hoverTextColor="hover:text-white" selectedColor="focus:bg-[#0085FF]" selectedTextColor="focus:text-white"/>
+              <ButtonFilter text="WhatsApp" svg="/whatsapp.svg" fill="bg-[#F0F0F0]" textColor="text-[#25D366]" hoverColor="hover:bg-green-500" hoverTextColor="hover:text-white" selectedColor="focus:bg-green-500" selectedTextColor="focus:text-white"/>
+              <ButtonFilter text="Telegram" svg="/telegram.svg" fill="bg-[#F0F0F0]" textColor="text-[#229ED9]" hoverColor="hover:bg-[#229ED9]" hoverTextColor="hover:text-white" selectedColor="focus:bg-[#229ED9]" selectedTextColor="focus:text-white"/>
+            </div>
           </div>
           <div className="proxy-list">
 
