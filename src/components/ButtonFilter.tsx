330cb8696730a70352b9daa542ddba19107030e8
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-01-08 19:16:34 -0800

    Default select all proxies button

--- src/components/ButtonFilter.tsx
+++ src/components/ButtonFilter.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import React from "react";
+import { useRef, useEffect } from "react";
 
 const ButtonFilter = (props) => {
     var svg;
@@ -10,9 +12,17 @@ const ButtonFilter = (props) => {
         svg = <div className="pl-3 sm:pl-4 lg:pl-6"></div>
     }
 
+    const focusButton = useRef(null);
+
+    useEffect(() => {
+        if (props.focusState == "true") {
+            focusButton.current.focus();
+        }
+    }, []);
+    
     return(
         <>
-        <button className={`flex proxy-button ${props.fill} ${props.hoverColor} ${props.textColor} ${props.hoverTextColor} ${props.selectedColor} ${props.selectedTextColor} py-2 rounded-xl w-28 sm:w-32 md:w-36 lg:w-44`}>
+        <button ref={focusButton} className={`flex proxy-button ${props.fill} ${props.hoverColor} ${props.textColor} ${props.hoverTextColor} ${props.selectedColor} ${props.selectedTextColor} py-2 rounded-xl w-28 sm:w-32 md:w-36 lg:w-44`}>
             <div className="flex items-center justify-center mx-auto">
                 {svg}
                 <div className="font-medium text-xs sm:text-sm md:text-md lg:text-lg pr-3 sm:pr-4 lg:pr-6">
