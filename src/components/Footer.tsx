6f4374a4596608a6ce2fcdbb44e1fcbe739fcaa8
Krish Shah <krish.life21@gmail.com>
2023-01-09 01:20:39 -0700

    New layout with faq

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,76 @@
-const Footer = () => {
-    return(
-        <>
-        <div className="flex flow-root justify-between footer relative p-3 bg-[#0085FF] text-white">
-            <div className="flex flex-row float-left justify-center items-center underline gap-2 sm:gap-6 text-[8px] sm:text-sm md:text-md lg:text-lg">
-                <a href="mailto:team@internetactivism.org" className="hover:text-blue-700">Contact Us</a>
-                <a href="https://www.internetactivism.org/about.html" className="hover:text-blue-700" target="_blank" rel="noopener noreferrer">About</a>
-                <a href="https://twitter.com/internetactvsm" className="hover:text-blue-700" target="_blank" rel="noopener noreferrer">Twitter</a>
-            </div>
-            <div className="float-right inset-y-0 right-0 items-end">
-                <p className="text-[8px] sm:text-sm md:text-md lg:text-lg text-white">Built by InternetActivism, a 501(c)(3) organization.</p>
-            </div>
+const Footer = (props) => {
+  if (props.light) {
+    return (
+      <>
+        <div className="flex flow-root justify-between footer relative p-3 bg-white text-[#0085FF]">
+          <div className="flex flex-row float-left justify-center items-center underline gap-2 sm:gap-6 text-[8px] sm:text-sm md:text-md lg:text-lg">
+            <a
+              href="mailto:team@internetactivism.org"
+              className="hover:text-blue-700"
+            >
+              Contact Us
+            </a>
+            <a
+              href="https://www.internetactivism.org/about.html"
+              className="hover:text-blue-700"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              About
+            </a>
+            <a
+              href="https://twitter.com/internetactvsm"
+              className="hover:text-blue-700"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+          </div>
+          <div className="float-right inset-y-0 right-0 items-end">
+            <p className="text-[8px] sm:text-sm md:text-md lg:text-lg text-[#0085FF]">
+              Built by InternetActivism, a 501(c)(3) organization.
+            </p>
+          </div>
         </div>
-        </>
+      </>
     );
+  }
+  return (
+    <>
+      <div className="flex flow-root justify-between footer relative p-3 bg-[#0085FF] text-white">
+        <div className="flex flex-row float-left justify-center items-center underline gap-2 sm:gap-6 text-[8px] sm:text-sm md:text-md lg:text-lg">
+          <a
+            href="mailto:team@internetactivism.org"
+            className="hover:text-blue-700"
+          >
+            Contact Us
+          </a>
+          <a
+            href="https://www.internetactivism.org/about.html"
+            className="hover:text-blue-700"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            About
+          </a>
+          <a
+            href="https://twitter.com/internetactvsm"
+            className="hover:text-blue-700"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+        </div>
+        <div className="float-right inset-y-0 right-0 items-end">
+          <p className="text-[8px] sm:text-sm md:text-md lg:text-lg text-white">
+            Built by InternetActivism, a 501(c)(3) organization.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
