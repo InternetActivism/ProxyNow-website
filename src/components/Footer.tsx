d88cb493a7068af825f572c835d4c60f5c200428
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-01-08 21:47:59 -0800

    Add proxy list header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
 const Footer = () => {
     return(
         <>
-        <div className="flex flow-root footer relative p-3 bg-[#0085FF] text-white">
-            <div className="flex flex-row float-left justify-center items-center underline gap-6 text-xs sm:text-sm md:text-md lg:text-lg">
+        <div className="flex flow-root justify-between footer relative p-3 bg-[#0085FF] text-white">
+            <div className="flex flex-row float-left justify-center items-center underline gap-2 sm:gap-6 text-[8px] sm:text-sm md:text-md lg:text-lg">
                 <a href="mailto:team@internetactivism.org" className="hover:text-blue-700">Contact Us</a>
                 <a href="https://www.internetactivism.org/about.html" className="hover:text-blue-700" target="_blank" rel="noopener noreferrer">About</a>
                 <a href="https://twitter.com/internetactvsm" className="hover:text-blue-700" target="_blank" rel="noopener noreferrer">Twitter</a>
             </div>
             <div className="float-right inset-y-0 right-0 items-end">
-                <p className="text-xs sm:text-sm md:text-md lg:text-lg text-white">Built by InternetActivism, a 501(c)(3) organization.</p>
+                <p className="text-[8px] sm:text-sm md:text-md lg:text-lg text-white">Built by InternetActivism, a 501(c)(3) organization.</p>
             </div>
         </div>
         </>
