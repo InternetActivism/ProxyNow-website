100965e340c4791cc4ee6c382d07317bc77c6f01
Adrian Gri <adrian.gri03@gmail.com>
2023-01-12 15:05:36 -0500

    Fix Mac screenshots sizing

--- src/components/MacScreenshots.tsx
+++ src/components/MacScreenshots.tsx
@@ -4,10 +4,10 @@ import terminal from '../assets/macos_terminal.png';
 
 const MacScreenshots = () => {
     return(
-        <div className="flex flex-col sm:flex-row sm:-mx-56 max-w-screen">
-            <Image className="max-w-screen" src={finder} alt='MacOS Finder' height={500} style={{ objectFit: 'contain' }} />
-            <Image className='hidden sm:block' src='/arrow.svg' alt='Arrow' width={100} height={50} />
-            <Image className="max-w-screen" src={terminal} alt="MacOS Terminal" height={500} style={{ objectFit: 'contain' }} />
+        <div className="flex flex-col justify-center items-center sm:flex-row sm:-mx-56 max-w-screen">
+            <Image className="w-full h-full sm:w-2/5" src={finder} alt='MacOS Finder' height={500} style={{ objectFit: 'contain' }} />
+            <Image className="hidden sm:block" src='/arrow.svg' alt='Arrow' width={100} height={50} />
+            <Image className="w-full h-full sm:w-2/5" src={terminal} alt="MacOS Terminal" height={500} style={{ objectFit: 'contain' }} />
         </div>
     );
 };
