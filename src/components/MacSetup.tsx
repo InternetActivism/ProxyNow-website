dc17c108b40dd8e9a8ffbfbfd39090c970a8edcb
Adrian Gri <adrian.gri03@gmail.com>
2023-01-27 13:09:35 -0500

    Fix register your proxy buttons

--- src/components/MacSetup.tsx
+++ src/components/MacSetup.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { setupCopy, troubleshootingSteps } from "../utils/constants";
 import ButtonBlue from "./ButtonBlue";
 import ButtonDownload from "./ButtonDownload";
@@ -19,7 +20,9 @@ const MacSetup = () => {
                 <MacScreenshots />
             </SetupStep>
             <SetupStep step={3} stepData={copy[2]} >
-                <ButtonBlue text={"Register Your Proxy"}  svgD="M9.47 5.47a.75.75 0 0 1 1.06 0l6 6a.75.75 0 0 1 0 1.06l-6 6a.75.75 0 1 1-1.06-1.06L14.94 12 9.47 6.53a.75.75 0 0 1 0-1.06Z"/>
+                <Link href="/register">
+                    <ButtonBlue text={"Register Your Proxy"}  svgD="M9.47 5.47a.75.75 0 0 1 1.06 0l6 6a.75.75 0 0 1 0 1.06l-6 6a.75.75 0 1 1-1.06-1.06L14.94 12 9.47 6.53a.75.75 0 0 1 0-1.06Z"/>
+                </Link>
             </SetupStep>
             <div className="flex flex-col w-full px-4 pb-0 sm:pt-10 sm:px-12 md:px-28 lg:px-36 mx-auto max-w-screen-xl">
                 <h1 className="font-semibold text-xl sm:text-2xl md:text-3xl text-black lg:text-4xl pt-20 pb-2">
