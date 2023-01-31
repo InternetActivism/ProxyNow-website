5fca13c2f53ac8289d003851316712992eec3d25
Krish Shah <krish.life21@gmail.com>
2023-01-31 09:24:02 -0500

    change footer copy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <>
       <div className="flex flow-root justify-between footer relative p-3 bg-[#0085FF] text-white">
         <div className="flex flex-row float-left justify-center items-center underline gap-2 sm:gap-6 text-[8px] sm:text-sm md:text-md lg:text-lg">
-        <a
+          <a
             href="https://www.internetactivism.org/contact"
             className="hover:text-blue-700"
             target="_blank"
@@ -25,19 +25,21 @@ const Footer = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Submit a Proxy
+            Share a Proxy
           </a>
         </div>
         <div className="float-right inset-y-0 right-0 items-end">
           <p className="text-[8px] sm:text-sm md:text-md lg:text-lg text-white">
-            An <a
-            href="https://internetactivism.org"
-            className="hover:text-blue-700 underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            InternetActivism
-          </a> project.
+            An{" "}
+            <a
+              href="https://internetactivism.org"
+              className="hover:text-blue-700 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              InternetActivism
+            </a>{" "}
+            project.
           </p>
         </div>
       </div>
